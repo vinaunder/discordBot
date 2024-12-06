c9f632429ce99d778a7b63250f28d735ca48ec15
Vinicius Costa <vinamsn@gmail.com>
2024-12-06 09:17:13 +0000

    ajust

--- keep_alive.js
+++ keep_alive.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const fetch = require('node-fetch'); // Instale o pacote: npm install node-fetch
 
 // Configurar o servidor HTTP
 const PORT = process.env.PORT || 8080;
@@ -9,25 +8,3 @@ http.createServer((req, res) => {
 }).listen(PORT, () => {
     console.log(`Servidor HTTP rodando na porta ${PORT}`);
 });
-
-// URL do servidor no Render
-const SERVER_URL = 'https://discordbot-6gd4.onrender.com/'; // Substitua pela URL do seu servidor
-
-// Função para auto-pingar o servidor
-function keepAlive() {
-    setInterval(async () => {
-        try {
-            const res = await fetch(SERVER_URL);
-            if (res.ok) {
-                console.log(`Ping bem-sucedido: ${new Date().toLocaleTimeString()}`);
-            } else {
-                console.error(`Falha no ping: ${res.status}`);
-            }
-        } catch (error) {
-            console.error(`Erro ao pingar o servidor: ${error.message}`);
-        }
-    }, 5 * 60 * 1000); // A cada 5 minutos
-}
-
-// Inicia o self-pinging
-keepAlive();
